1231812a5565c1d8e52d1603f843158657ad1466
Xia Dong <dongxiapp@gmail.com>
2019-03-12 19:45:05 +0000

    fill urls_index.ejs

--- expressServer.js
+++ expressServer.js
@@ -20,6 +20,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/urls", (req, res) => {
+//   let templateVars = { urls: urlDatabase };
+//   res.render("urls_index", templateVars);
+// });
+
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
