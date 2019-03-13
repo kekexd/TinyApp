b060c7adba9712e76c33311629681982b1188866
Xia Dong <dongxiapp@gmail.com>
2019-03-13 00:23:37 +0000

    Add a function to generate random shortURL

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,9 @@ var app = express();
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -42,3 +45,15 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let randomURL = Math.random().toString(36).substr(2, 6);
+  console.log(randomURL);
+}
+
+//generateRandomString();
